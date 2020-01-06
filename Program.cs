e3dd77b7a998cbf86486e6149d4d1ff52fec864a
Ross <petersonrl1@gmail.com>
2020-01-06 09:42:59 -0500

    updates

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace WarCardGame
     {
         static void Main(string[] args)
         {            
-            Console.SetWindowSize(100, 50);
+            Console.SetWindowSize(100, 40);
             Console.WriteLine("Welcome to the WAR CARD GAME\n");
 
-            Console.WriteLine("|| HOW TO PLAY:\n")                            
+            Console.WriteLine("|| HOW TO PLAY:\n");                            
             Console.WriteLine(" - You and the computer are dealt one half of a shuffled deck of cards.".PadLeft(1));
             Console.WriteLine(" - Each turn, each player draws a card from their deck and lays it face up.".PadLeft(1));
             Console.WriteLine(" - The player that draws the card with higher value gets both cards.".PadLeft(1));
@@ -58,12 +58,12 @@ namespace WarCardGame
                 while (!yourDeck.IsEmpty() && !computerDeck.IsEmpty())
                 {
                     Console.ReadLine();
-
                     // player and computer both draw cards
                     WarCard playerDraw = yourDeck.DrawCard();
                     WarCard computerDraw = computerDeck.DrawCard();
                     totalHands++;
-                    //0 is the suit, 1 is the face
+                    //0 is the suit, 1 is the face as set in Card.cs
+                    Console.Clear();
                     Console.WriteLine("Player has drawn: {0} of {1}.\n", playerDraw.face, playerDraw.suit);
                     Console.WriteLine("Computer has drawn: {0} of {1}.\n", computerDraw.face, computerDraw.suit);
 
@@ -77,12 +77,23 @@ namespace WarCardGame
                         Console.WriteLine("The Computer has won this hand.\nThe cards have been added to the computer's deck.\n");
                         computerDeck.PlaceInDeck(playerDraw, computerDraw);
                     }
+                    //WAR CASE: when there's a draw, each player draws 5 cards, and the 5th card for each player decides
+                    //who wins all the cards drawn in the hand. Whoever has the higher face value card with the 5th card
+                    //will add all the 
+                    //cards their opponent drew in the hand (5) plus the original card that was drawn that caused the War
+                    //Case. The winner will also add all cards the winner drew in the War Case + the initial card.
+
+                    //THE REASON the game can end on a draw is because once a player or comp deck gets to 1 card and they 
+                    //play it, there deck is now empty and the next hand they have no cards. Something more eleborate 
+                    //would have to be coded to handle a war case, and a war case when the player/comp has less than the 
+                    //amount of cards used in the war case.
+                    
+                    //the War logic would go here in the else block below.
                     else
                     {
                         Console.WriteLine("It's a draw!\n");
                     }
                 }
-
                 if (yourDeck.IsEmpty())
                 {
                     Console.WriteLine("After a total of {0} hands, the player has won!\n\n", totalHands);
@@ -91,7 +102,6 @@ namespace WarCardGame
                 {
                     Console.WriteLine("After a total of {0} hands, the computer has won!\n\n", totalHands);
                 }
-                Console.Clear();
                 string userInput;
                 do
                 {
@@ -102,6 +112,12 @@ namespace WarCardGame
                 if (userInput == "n")
                 {
                     break;
+                }
+                else
+                {
+                    Console.WriteLine("\nGreat! Let's play again!\n");
+                    Console.WriteLine("|| CONTROLS:\n");
+                    Console.WriteLine(" - Press enter to start the next hand, and both cards for the user and computer will be drawn\n".PadLeft(1));
                 }
             }           
         }  
